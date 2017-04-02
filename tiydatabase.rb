65011707a4d005a16e856fd51debd606d2d4d910
Angela Coates <angela@angelamarii.com>
2017-04-01 22:55:09 -0400

    still working on form problem with connection to database

--- tiydatabase.rb
+++ tiydatabase.rb
@@ -12,17 +12,36 @@ get '/employees' do
 employees_db = PG.connect(dbname:"tiy-database")
 @employees = employees_db.exec("select * from employees")
 
-  # in order to list the , we have to get them from the database...
-
   erb :employees
 end
 
 get '/show_info' do
   employees_db = PG.connect(dbname:"tiy-database")
 
-  fuck = params["id"]
+  id = params["id"]
 
-  @employees = employees_db.exec("select * from employees where id = $1", [fuck])
+  @employees = employees_db.exec("select * from employees where id = $1", [id])
 
   erb :show_info
+end
+
+get '/add_employee' do
+  addemployee_db = PG.connect(dbname: "tiy-database")
+
+  name = params["name"]
+  phone = params["phone"]
+  address = params["address"]
+  position = params["position"]
+  salary = params["salary"]
+  github = params["github"]
+  slack = params["slack"]
+  addemployee_db = PG.connect(dbname: "tiy-database")
+  @employees = addemployee_db.exec("INSERT INTO employees(name, phone, address, position, salary, github, slack) VALUES($1, $2, $3, $4, $5, $6, $7)", [name, phone, address, position, salary, github, slack])
+
+
+
+
+  erb :add_employee
+# #
+
 end
